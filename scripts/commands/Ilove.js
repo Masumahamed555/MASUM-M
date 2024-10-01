691f192dd0ef68f3f4c655d4f314650104d699d8
𝐌𝐀𝐒𝐔𝐌 𝐀𝐇𝐀𝐌𝐄𝐃 <ahamedmdmasum03@gmail.com>
2024-10-01 09:40:17 +0600

    Update Ilove.js

--- scripts/commands/Ilove.js
+++ scripts/commands/Ilove.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("I love you")==0 || (event.body.indexOf("i love you")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
 		var msg = {
-				body: "-❒  ভালোবাসা নামক আব্লামি করতে মন চাইলে চলে জাও আরিয়ান বস  এর ইনবক্সে -!!"
+				body: "-❒  ভালোবাসা নামক আব্লামি করতে মন চাইলে চলে জাও বস মাসুম এর ইনবক্সে চলে জাও -!!"
     }
 			api.sendMessage(msg, threadID, messageID);
 		}
