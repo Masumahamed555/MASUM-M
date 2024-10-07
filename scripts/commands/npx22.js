e60924b3f8f9b76052a59c573fb53c49137af84c
𝐌𝐀𝐒𝐔𝐌 𝐀𝐇𝐀𝐌𝐄𝐃 <ahamedmdmasum03@gmail.com>
2024-10-07 20:53:34 +0600

    Update npx22.js

--- scripts/commands/npx22.js
+++ scripts/commands/npx22.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "npx31",
+	name: "npx22",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("🎵")==0 || body.indexOf(" ")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf("🕵️‍♀️")==0 || body.indexOf("🙂")==0 || body.indexOf(" ")==0) {
 		var msg = {
-				body: "জান মন খারাপ করো আমি সব সময় তোমাকে ভালোবাসি-🤗 - 𝐂𝐑𝐄𝐀𝐓𝐎𝐑 𝐀𝐍𝐃 𝐕𝐎𝐈𝐂𝐄 𝐎𝐅 𝐅𝐀𝐇𝐈𝐌 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘 - 😇🫶🫠",
+				body: "জান মন খারাপ করো আমি সব সময় তোমাকে ভালোবাসি-🙂",
 				attachment: fs.createReadStream(__dirname + `/Nayan/ssstik.io_1721891866438.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
@@ -28,4 +28,4 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	start: function({ nayan }) {
 
   }
-        }
+}
