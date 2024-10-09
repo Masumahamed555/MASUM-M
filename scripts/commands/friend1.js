84276ffa43c8314dda133d05c29d0be65922ac2c
𝐌𝐀𝐒𝐔𝐌 𝐀𝐇𝐀𝐌𝐄𝐃 <ahamedmdmasum03@gmail.com>
2024-10-09 12:03:09 +0600

    Update friend1.js

--- scripts/commands/friend1.js
+++ scripts/commands/friend1.js
@@ -17,7 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
-  const NAYAN = ['https://i.imgur.com/iyNLH0u.mp4']
+  const NAYAN = ['https://i.imgur.com/iyNLH0u.mp4','https://i.imgur.com/3HVPluM.mp4','https://i.imgur.com/jE1VSA2.mp4']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(
@@ -26,7 +26,7 @@ const media = (
     )
   ).data;
 
-  if (body.indexOf("🤍")==0 || body.indexOf("🤝")==0 || body.indexOf("🫶")==0 ||body.indexOf("🤝")==0) {
+  if (body.indexOf("🤍")==0 || body.indexOf("🤝")==0 || body.indexOf("🫶")==0 ||body.indexOf("☺️")==0) {
     var msg = {
         body: "•❊ ༅⎯͢🩷𝐌𝐀𝐒𝐔𝐌🩷 ❊\n⎯͢𝗕𝗘𝗦𝗧 𝗙𝗥𝗜𝗘𝗡𝗗💘👩🏻‍🤝‍👩🏼🤝_",
         attachment: media
