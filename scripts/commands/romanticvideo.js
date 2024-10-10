3a271b7226478bf7e63557324ca4a91715396800
𝐌𝐀𝐒𝐔𝐌 𝐀𝐇𝐀𝐌𝐄𝐃 <ahamedmdmasum03@gmail.com>
2024-10-10 12:21:05 +0600

    Update romanticvideo.js

--- scripts/commands/romanticvideo.js
+++ scripts/commands/romanticvideo.js
@@ -25,7 +25,18 @@ module.exports.run = async ({ api, event, args, client, Users, Threads, __GLOBAL
 
   var link = [
     "https://i.imgur.com/xHb1fAb.mp4",
-    "https://i.imgur.com/Dfr2WBS.mp4"
+    "https://i.imgur.com/Dfr2WBS.mp4"'
+    "https://i.imgur.com/ZszXvms.mp4",
+    "https://i.imgur.com/p1oTrOR.mp4",
+    "https://i.imgur.com/1FfSEr9.mp4",
+    "https://i.imgur.com/AzPnBzv.mp4",
+    "https://i.imgur.com/c3XFSZD.mp4",
+    "https://i.imgur.com/nHZ4N15.mp4",
+    "https://i.imgur.com/uMTnmF3.mp4",
+    "https://i.imgur.com/j4URGzq.mp4",
+    "https://i.imgur.com/lo59II6.mp4",
+    "https://i.imgur.com/1Kx2h6k.mp4",
+    "https://i.imgur.com/gyEEGJ4.mp4"
   ];
 
   var callback = () => {
